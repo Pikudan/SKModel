57b7eac96103d7527a755b11c88c47edcc0b3af4
Daniil Pikurov <89961174+Pikudan@users.noreply.github.com>
2025-12-16 00:44:32 +0300

    Delete scripts directory

--- scripts/run_mpi_cuda_2gpu_2400_3600.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Скрипт запуска MPI+CUDA программы на кластере
-# Использование: mpisubmit.pl -p 2 --gpu 2 00:05 ./main_mpi_cuda -- 2400 3600
-
-mpisubmit.pl -p 2 --gpu 2 00:05 ./main_mpi_cuda -- 2400 3600
-
